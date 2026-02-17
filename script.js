e217d0fdd8f6f4c28bb7ea4cc1ea026217b9fc27
WujecDamian <wujecdamian12@gmail.com>
2026-02-18 00:39:49 +0100

    feat: add password validations script

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const password1 = document.querySelector('#password')
 const password2 = document.querySelector('#conf-password')
+const errorParagraph = document.querySelector('#password--error')
+const submitBtn = document.querySelector('#submit-btn')
+
+submitBtn.addEventListener('click', () => {
+  if (password1.value === password2.value) {
+    password1.setAttribute('style', 'border-color:green')
+    password2.setAttribute('style', 'border-color:green')
+    errorParagraph.innerText = ''
+  } else {
+    errorParagraph.innerText = 'Password does not match!'
+    password1.setAttribute('style', 'border-color:red')
+    password2.setAttribute('style', 'border-color:red')
+  }
+})
